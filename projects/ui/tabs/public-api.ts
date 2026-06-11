5fa35dcc96eaed3fa69108f7390b3845d3fb2900
Christian Weiss <eiweiss@users.noreply.github.com>
2026-06-11 08:39:09 +0200

    feat(ui/tabs): add CngxTabNav organism for native routerLink tab bars

--- projects/ui/tabs/public-api.ts
+++ projects/ui/tabs/public-api.ts
@@ -2,4 +2,5 @@
  * @module @cngx/ui/tabs
  */
 export { CngxTabGroup } from './tab-group.component';
+export { CngxTabNav } from './tab-nav.component';
 export { CngxTabOverflow } from './tab-overflow.component';
