87d444fa5d41f0162447f8cf7871b551d7174c51
Christian Weiss <eiweiss@users.noreply.github.com>
2026-08-18 07:53:11 +0200

    feat(eslint-plugin): add model-for-two-way rule

--- packages/eslint-plugin/src/plugin.ts
+++ packages/eslint-plugin/src/plugin.ts
@@ -3,6 +3,7 @@ import { RULE_METADATA, type RuleId, type RuleSeverity } from './metadata';
 import { noEffectInNgOnInit } from './rules/no-effect-in-ngoninit';
 import { untrackedInEffect } from './rules/untracked-in-effect';
 import { noBehaviorsubjectLocalState } from './rules/no-behaviorsubject-local-state';
+import { modelForTwoWay } from './rules/model-for-two-way';
 
 /**
  * The `@cngx/eslint-plugin` plugin object.
@@ -22,6 +23,7 @@ export const plugin: TSESLint.FlatConfig.Plugin = {
     'no-effect-in-ngoninit': noEffectInNgOnInit,
     'untracked-in-effect': untrackedInEffect,
     'no-behaviorsubject-local-state': noBehaviorsubjectLocalState,
+    'model-for-two-way': modelForTwoWay,
   },
 };
 
